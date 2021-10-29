4300c176179b4823a44061adeece58fd4a987f7c
cattlenotpets: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 <you@example.com>
2021-10-29 15:50:10 +0530

    modified code add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 
 ################### EC2 ########################################
 resource "aws_instance" "ec2_1" {
-  ami           = "ami-02e136e904f3da870"
-  instance_type = "t2.micro"
-  subnet_id     = data.aws_subnet.selected.id
-  #key_name = var.my_key
+  ami           = var.ami_name
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnet.selected.id 
   tags = {
     Name = "ap_instance"
   }
--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,11 +1,21 @@
 
+#################################### variable.tf #############################################
+
 variable "subnet_name" {
   description = "This is the name of the subnet"
   type = string
   default = "subnet_1"
 }
 
-#variable "my_key" {
-#  type = string
-#  default = "dev-account"
-#}
+variable "ami_name" {
+  description = "This is the name of the ami"
+  type = string
+  default = "ami-02e136e904f3da870"
+}
+
+variable "instance_type" {
+
+  description = "This is the name of the instance_type"
+  type = string
+  default = "t2.micro"
+}
