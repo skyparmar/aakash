0e408c1221e26fd7c12db8efa13ed0d22c528cde
skyparmar <90676654+skyparmar@users.noreply.github.com>
2021-11-25 15:20:29 +0530

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,95 +9,95 @@ resource "aws_instance" "ec2_1" {
   }
 }
 
-###########  VPC block ##################
+# ###########  VPC block ##################
 
-resource "aws_vpc" "myVpc" {
-  cidr_block = "10.0.0.0/16"
-}
+# resource "aws_vpc" "myVpc" {
+#   cidr_block = "10.0.0.0/16"
+# }
 
-##########  Internet Gateway ############
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myVpc.id
+# ##########  Internet Gateway ############
+# resource "aws_internet_gateway" "igw" {
+#   vpc_id = aws_vpc.myVpc.id
 
-  tags = {
-    Name = "igw" 
-  }
-}
+#   tags = {
+#     Name = "igw" 
+#   }
+# }
 
-######### Subnets #################
+# ######### Subnets #################
 
-resource "aws_subnet" "mySubnet" {
-  vpc_id     = aws_vpc.myVpc.id 
-  cidr_block = "10.0.1.0/24"
+# resource "aws_subnet" "mySubnet" {
+#   vpc_id     = aws_vpc.myVpc.id 
+#   cidr_block = "10.0.1.0/24"
 
-    tags = {
-      Name = "subnet"
-    }
-}
+#     tags = {
+#       Name = "subnet"
+#     }
+# }
 
-############ Route Table ###################
+# ############ Route Table ###################
 
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.myVpc.id
+# resource "aws_route_table" "rt" {
+#   vpc_id = aws_vpc.myVpc.id
 
-  route = []
-  tags = {
-    Name = "example"
-  }
-}
+#   route = []
+#   tags = {
+#     Name = "example"
+#   }
+# }
 
-################### Route #####################
+# ################### Route #####################
 
-resource "aws_route" "route" {
-  route_table_id         = aws_route_table.rt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-  depends_on             = [aws_route_table.rt]
-}
+# resource "aws_route" "route" {
+#   route_table_id         = aws_route_table.rt.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.igw.id
+#   depends_on             = [aws_route_table.rt]
+# }
 
-################# Route Table Associations #################
+# ################# Route Table Associations #################
 
-resource "aws_route_table_association" "a1" {
-  subnet_id      = aws_subnet.mySubnet.id
-  route_table_id = aws_route_table.rt.id
-}
+# resource "aws_route_table_association" "a1" {
+#   subnet_id      = aws_subnet.mySubnet.id
+#   route_table_id = aws_route_table.rt.id
+# }
 
-##################### Security Group ###################
+# ##################### Security Group ###################
 
-resource "aws_security_group" "sg" {
-  name        = "allow_all_traffic"
-  description = "Allow all inbound traffic"
-  vpc_id      = aws_vpc.myVpc.id
+# resource "aws_security_group" "sg" {
+#   name        = "allow_all_traffic"
+#   description = "Allow all inbound traffic"
+#   vpc_id      = aws_vpc.myVpc.id
 
-  ingress = [
-    {
-      description      = "All traffic"
-      from_port        = 0    # All ports
-      to_port          = 0    # All Ports
-      protocol         = "-1" # All traffic
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = null
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
-    }
-  ]
+#   ingress = [
+#     {
+#       description      = "All traffic"
+#       from_port        = 0    # All ports
+#       to_port          = 0    # All Ports
+#       protocol         = "-1" # All traffic
+#       cidr_blocks      = ["0.0.0.0/0"]
+#       ipv6_cidr_blocks = null
+#       prefix_list_ids  = null
+#       security_groups  = null
+#       self             = null
+#     }
+#   ]
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      description      = "Outbound rule"
-      prefix_list_ids  = null
-      security_groups  = null
-      self             = null
-    }
-  ]
+#   egress = [
+#     {
+#       from_port        = 0
+#       to_port          = 0
+#       protocol         = "-1"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#       ipv6_cidr_blocks = ["::/0"]
+#       description      = "Outbound rule"
+#       prefix_list_ids  = null
+#       security_groups  = null
+#       self             = null
+#     }
+#   ]
 
-  tags = {
-    Name = "all_traffic"
-  }
-}
+#   tags = {
+#     Name = "all_traffic"
+#   }
+# }
