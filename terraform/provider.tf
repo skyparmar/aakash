3037d605bc45eca6c98ff24a345f4456eb9cdc24
skyparmar <90676654+skyparmar@users.noreply.github.com>
2021-11-27 17:46:55 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,23 @@
 ############################ provider.tf #####################################################
-provider "aws" {
-  alias  = "us-east-2"
-  region = "us-east-1"
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.57.0"
+    }
+  }
 }
+
+provider "aws" {
+  region = "us-east-1"
+   
+}
+
+# provider "aws" {
+#   alias  = "us-east-2"
+#   region = "us-east-1"
+
+# }
 
 # terraform {
 #   required_providers {
