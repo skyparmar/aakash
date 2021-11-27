33d2c9bc596472b2f3c7576874fb85a630d8dc22
skyparmar <90676654+skyparmar@users.noreply.github.com>
2021-11-27 17:26:09 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,13 +1,13 @@
 ############################ provider.tf #####################################################
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-provider "aws" {
-  region = "us-east-1"
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
+# provider "aws" {
+#   region = "us-east-1"
 
-}
+# }
